8dd62db9d271b3c571c0f3806509ced4c041ce49
Nayan1234l <ali15-13943@diu.edu.bd>
2024-04-29 16:15:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='justify-center items-center p-16'>Welcome to Savvy Movies</h1>
+      <p>It's platform where you can find all the popular and latest movies latest movies  </p>
+    </>
+  )
+}
+
+export default App
